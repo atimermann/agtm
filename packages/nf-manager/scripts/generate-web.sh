3721033049a9a1898854c5ea63dacd4e8faa45fe
André Timermann <andre@timermann.com.br>
2023-10-25 21:19:23 -0300

    update pacakge.json

--- packages/nf-manager/scripts/generate-web.sh
+++ packages/nf-manager/scripts/generate-web.sh
@@ -2,7 +2,8 @@
 
 # Este script gera o código estático do nuxt, copia para o assets do app main e executa install-assets copiando para o diretório public
 
-(cd web && npm run generate)
-rm -rf src/apps/main/assets
-cp -r web/.output/public src/apps/main/assets
-npm run install-assets
+# TODO: Alterar script para pegar web do projeto nf-manager-web, foi removido da pasta web
+#(cd web && npm run generate)
+#rm -rf src/apps/main/assets
+#cp -r web/.output/public src/apps/main/assets
+#npm run install-assets

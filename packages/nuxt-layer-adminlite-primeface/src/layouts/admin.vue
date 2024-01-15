78de9c3e498a444766ec9d92562156f9990f95aa
André Timermann <andre@timermann.com.br>
2024-01-15 15:50:06 -0300

    correção import useRunTimeConfig

--- packages/nuxt-layer-adminlite-primeface/src/layouts/admin.vue
+++ packages/nuxt-layer-adminlite-primeface/src/layouts/admin.vue
@@ -47,7 +47,7 @@
 // TODO: Documentar tudo aqui e no zim (De preferencia criar cheatChet)
 // import {MenuItem, SubMenuItem} from '@agtm/nuxt-layer-adminlte-primevue'
 
-import { useAppConfig, ref, provide } from '#imports'
+import { useAppConfig, ref, provide, useRuntimeConfig } from '#imports'
 
 import SideBarMenu from '../components/admin/SideBarMenu.vue'
 import TopNavbar from '../components/admin/TopNavbar.vue'

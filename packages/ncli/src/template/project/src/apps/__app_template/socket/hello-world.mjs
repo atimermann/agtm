07f2a1cbae2632f2d3ba188438f479657eb045f7
André Timermann <andre@timermann.com.br>
2024-02-22 23:53:57 -0300

    @agtm/ncli
    - Atualização template para @agtm/node-framework

--- packages/ncli/src/template/project/src/apps/__app_template/socket/hello-world.mjs
+++ packages/ncli/src/template/project/src/apps/__app_template/socket/hello-world.mjs
@@ -1,32 +1,42 @@
 /**
- * **Created on 05/01/2024**
- *
- * apps/main/controllers/helloWorld.mjs
- *
- * @author André Timermann <andre@timermann.com.br>
- *
  * @file Helloworld Demo
  */
-import { SocketController, createLogger, Config } from '@agtm/node-framework'
-
+import { Config, createLogger, SocketController } from '@agtm/node-framework'
 const logger = createLogger('HelloWorld')
 
 /**
  *
  */
 export default class HelloWorldController extends SocketController {
+  namespace = '/hello-world'
+
   /**
    * Sockets.
    */
   async setup () {
-    this.namespace('/my-namespace').on('connection', socket => {
-      socket.emit('newData', { nane: 'João' })
+    // Get Prisma DSN
+    logger.debug(Config.get('myconfig'))
+
+    //  Events
+    this.on('product:create', async (data) => {
+      return true
     })
 
-    this.namespace('/admin', socket => {
-      socket.on('delete user', () => {
-        // ...
-      })
+    this.on('product:get', async (data) => {
+      return { name: 'New Product' }
+    })
+
+    // Direct access
+
+    // Socket.IO instance Object
+    this.io.on('connection', socket => {
+      socket.emit('hello', 1, '2', { 3: '4', 5: Buffer.from([6]) })
+    })
+
+    // Socket.io server namespaced
+    this.nsp.on('connection', async socket => {
+      socket.emit('hello', 1, '2', { 3: '4', 5: Buffer.from([6]) })
+      logger.debug(`Clientes connected: ${this.nsp.sockets.size}`)
     })
   }
 }

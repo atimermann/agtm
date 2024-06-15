bfd1170477819e6c9df5280cc2be929d021317e2
André Timermann <andre@timermann.com.br>
2024-06-15 00:06:48 -0300

    update packages

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-cd public || exit 1
-for dir in */ ; do
-  cd "$dir" || continue
-  echo "npm link $dir"
-  npm link
+for main_dir in public private ; do
+  cd "$main_dir" || exit 1
+  for dir in */ ; do
+    cd "$dir" || continue
+    echo "npm link $dir"
+    npm link
+    cd ..
+  done
   cd ..
 done
